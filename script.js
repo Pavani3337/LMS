3752e10f8e0caddc6573fc0d09f77bb7af0ab56b
pavani2007 <pavanichandrapatla@gmail.com>
2026-06-03 21:20:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ function initStorage() {
 
 window.onload = function () {
     initStorage();
-
-    isAdminLoggedIn = false;
-
-    document.getElementById("loginPage").classList.remove("hidden");
-    document.getElementById("dashboardPage").classList.add("hidden");
-
-    hideSections();
 };
 
 
